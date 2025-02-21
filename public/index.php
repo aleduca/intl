0b1fa0360e5c39a6cd973dd9f1f626957a922555
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2025-02-21 08:21:02 -0300

    chore(index) - Added some comments in index.php

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ date_default_timezone_set('America/Sao_Paulo');
 require __DIR__ . '/../vendor/autoload.php';
 
 // INTL - Funções de internacionalização
+// some changes here
 
 // Number Format - currency, data, decimal, porcentagem
 
