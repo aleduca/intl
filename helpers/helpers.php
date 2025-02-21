2e984c87827d8622605a6c284a35e11d626854f1
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2025-02-21 14:35:23 -0300

    Fix(Type) - Type hint added to datetime function

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use DateTime;
+
 /**
  * Convert date time format
  *
@@ -7,7 +9,7 @@
  * @param string $format
  * @return string
  */
-function dateTime($timeString, $format = 'd-m-Y H:i:s'): string
+function dateTime(string $timeString, string $format = 'd-m-Y H:i:s'): string
 {
   return DateTime::createFromFormat('Y-m-d H:i:s', $timeString)->format($format);
 }
