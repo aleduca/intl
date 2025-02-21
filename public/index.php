632e1048092f37e62f2fb3c4996bc9ec292e355e
Alexandre Cardoso <aleducardoso@gmail.com>
2025-02-21 07:38:40 -0300

    First commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// INTL - Funções de internacionalização
+
+// Number Format - currency, data, decimal, porcentagem
+
+// Calendar
+
+// Message Formatter
