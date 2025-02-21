31f18205092411ffd5ce121ef8a36cc38711ae8e
Alexandre Eduardo Cardoso <alecar2007@gmail.com>
2025-02-21 14:26:29 -0300

    Fix(DateTime) - The function now return with format method.

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,7 +1,7 @@
 <?php
 
 // datetime function
-function dateTime($timeString)
+function dateTime($timeString, $format = 'd-m-Y H:i:s')
 {
-  return DateTime::createFromFormat('Y-m-d H:i:s', $timeString);
+  return DateTime::createFromFormat('Y-m-d H:i:s', $timeString)->format($format);
 }
